491c75a161d1b7f374b0910edf18272619bf0f4e
schahinbekir <bsbekirsahinbs@gmail.com>
2022-12-15 05:45:10 +0300

    Contact Us, About Us done.

--- ECommerce/Controllers/AboutUsController.cs
+++ ECommerce/Controllers/AboutUsController.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Controllers
 {
+    [AllowAnonymous]
     public class AboutUsController : Controller
     {
-        [AllowAnonymous]
+        AboutUsManager aum = new AboutUsManager(new EfAboutUsRepository());
         public IActionResult Index()
         {
-            return View();
+            var value = aum.GetListAll().Where(x=>x.IsDeleted==false).ToList();
+            return View(value);
         }
     }
 }
--- ECommerce/Controllers/ContactUsController.cs
+++ ECommerce/Controllers/ContactUsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Controllers
@@ -6,9 +9,20 @@ namespace ECommerce.Controllers
     [AllowAnonymous]
     public class ContactUsController : Controller
     {
+        ContactUsManager cum = new ContactUsManager(new EfContactUsRepository());
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ContactUs cus)
+        {
+            cus.CreatedDate = DateTime.Now;
+            cus.UpdatedDate = DateTime.Now;
+            cus.IsDeleted = false;
+            cum.TAdd(cus);
+            return RedirectToAction("Index", "AboutUs");
         }
     }
 }
